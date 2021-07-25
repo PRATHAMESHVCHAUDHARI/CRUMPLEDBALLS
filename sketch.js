648a878245c17de9936079cb628430bf36c96226
PRATHAMESHVCHAUDHARI <84676564+PRATHAMESHVCHAUDHARI@users.noreply.github.com>
2021-07-25 23:24:30 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,7 +13,7 @@ function setup(){
     engine=Engine.create();
     world=engine.world;
 
-    var canvas=createCanvas(1260,600);
+    var canvas=createCanvas(1360,670);
     ground=new Ground(600,375,1200,50);
 
     
